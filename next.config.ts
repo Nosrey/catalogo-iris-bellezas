64e5756930379ebd31688552e045f3d1f34c1236
Nosrey <nosrey135@gmail.com>
2026-05-09 02:25:45 -0400

    fix de la ruta base

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'export',
-  basePath: '/catalogo-iris-bellezas',
-  assetPrefix: '/catalogo-iris-bellezas',
   images: {
     unoptimized: true,
     remotePatterns: [
